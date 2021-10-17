1efdceafc141f907e17388974144ff729d1bc96f
Ashiph <Ashiph@DESKTOP-LC027P8>
2021-10-17 15:59:48 +0530

    Refactoring the Empty String and Return Single value

--- StringCalculator/src/IncubyteTest/StringCalculator.java
+++ StringCalculator/src/IncubyteTest/StringCalculator.java
@@ -9,10 +9,18 @@ public class StringCalculator
 	 public int add(String input) {
 	    	
 	    	
-	        if(input.isEmpty()) {
+		 if(isEmpty(input)) {
 	        	return 0;
 	        }
 	       return Integer.parseInt(input);
 	    }
 	
+	 private boolean isEmpty(String input) {
+	    	return input.isEmpty();
+	    }
+	    
+	    private int stringToInt(String input) {
+	    	 return Integer.parseInt(input);
+	    }
+	 
 }
